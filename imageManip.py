d34a28cd2c6ea75190a29833165ae3b5d577952d
✯eMan aNsar✯ <87119440+Ema-Ans@users.noreply.github.com>
2023-05-30 11:52:18 +0300

    Added the load helper function

---
+++ imageManip.py
@@ -0,0 +1,21 @@
+import math
+
+import numpy as np
+from PIL import Image
+from skimage import color, io
+
+
+def load(image_path):
+    """Loads an image from a file path.
+
+    HINT: Look up `skimage.io.imread()` function.
+
+    Args:
+        image_path: file path to the image.
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+    out = io.imread(image_path)
+    out = out.astype(np.float64) / 255
+    return out
